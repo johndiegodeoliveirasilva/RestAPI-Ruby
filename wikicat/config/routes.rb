369f6388852f81570f68d5eaca9dc2897b061d35
John Diego <johnoliveirasilva9@gmail.com>
2021-08-26 23:20:55 -0300

    Change Resource to Get Route

--- wikicat/config/routes.rb
+++ wikicat/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :category, only: %i(show)
-      resources :graph, only: %i(show)
+      get 'category/:category', to: 'category#show'
+      get 'graph/:graph', to: 'graph#show'
     end
   end
 end
