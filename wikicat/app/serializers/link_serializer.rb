1f317bc4974645e6eded542c4afa886c211252bd
John Diego <johnoliveirasilva9@gmail.com>
2021-08-26 23:21:34 -0300

    Serialize Link

--- wikicat/app/serializers/link_serializer.rb
+++ wikicat/app/serializers/link_serializer.rb
@@ -1,3 +1,8 @@
 class LinkSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :sub_category
+
+  def sub_category
+    CGI.escape(object.cl_sortkey.force_encoding('ISO-8859-1')
+        .encode('utf-8', replace: nil).downcase.tr(' ', '_'))
+  end
 end
