741c4c6b6581d7afda52e81d9a4e1b1003fb8cc4
John Diego <johnoliveirasilva9@gmail.com>
2021-09-12 12:28:56 -0300

    Add Check show Category

--- wikicat/app/controllers/api/v1/category_controller.rb
+++ wikicat/app/controllers/api/v1/category_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::CategoryController < ApplicationController
   def show
-    category = params[:category] ? params[:category] : 'sports'
+    category = params[:category] ? params[:category] : 'Main_topic_classifications'
     @category = Category.where(cat_title: category.capitalize).first
-    render json: @category,
-           serializer: CategorySerializer,
-           root: 'category'
+    
+    if @category
+      render json: @category,
+            serializer: CategorySerializer,
+            root: 'category'
+    else
+      render json: { error: { text: '404 Not Found', status: 404}}
+    end
   end
 end
